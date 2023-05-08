d310bbbb6ac8653aa9319e60b87e3021fb3edfe0
Robert Kiewisz <56911280+RRobert92@users.noreply.github.com>
2023-05-08 09:11:20 +0200

    program update

--- js/calendar.js
+++ js/calendar.js
@@ -1,5 +1,16 @@
-$(function () {
-  $('.example-popover').popover({
-    container: 'body'
-  })
-})
+const eventBtns = document.querySelectorAll('.event-btn');
+const closeBtns = document.querySelectorAll('.close-btn');
+
+eventBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const details = btn.nextElementSibling;
+    details.style.display = 'block';
+  });
+});
+
+closeBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const details = btn.parentNode;
+    details.style.display = 'none';
+  });
+});
